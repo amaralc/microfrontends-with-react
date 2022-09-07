171d03c159aad56b2bf084f1ada56fa87a19d064
amaralc <amaral.calil@gmail.com>
2022-09-07 18:00:13 -0300

    feat: configure webpack to get content from remote entry

--- ecommerce-with-microfrontends/container/webpack.config.js
+++ ecommerce-with-microfrontends/container/webpack.config.js
@@ -1,4 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugion = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
     mode: 'development',
@@ -6,6 +7,12 @@ module.exports = {
         port: 8080
     },
     plugins: [
+        new ModuleFederationPlugion({
+            name: 'container',
+            remotes: {
+                products: 'products@http://localhost:8081/remoteEntry.js'
+            }
+        }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
         })
